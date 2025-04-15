4fd183f5a1f7d04c02792da267bdfa0442f28e7c
schmitz-eline <eline.schmitz@student.hepl.be>
2025-04-15 16:32:48 +0200

    correction in class

---
+++ src/ts/FallingObstacle.ts
@@ -0,0 +1,42 @@
+import {Circle} from "./framework25/shapes/Circle";
+import {randomInt} from "./framework25/helpers/random";
+import {Hsl} from "./framework25/colors/Hsl";
+import {settings} from "./settings";
+
+export class FallingObstacle extends Circle {
+    private canvas: HTMLCanvasElement;
+    private hue: number;
+    private speed: number;
+
+    constructor(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, hue: number) {
+        super(ctx, {
+            x: 0,
+            y: 0,
+        }, new Hsl(hue, randomInt(1, 100), randomInt(1, 100)), randomInt(settings.minRadius, settings.maxRadius));
+        this.canvas = canvas;
+        this.hue = hue
+        this.initValues();
+    }
+
+
+    private initValues() {
+        this.radius = randomInt(settings.minRadius, settings.maxRadius);
+        this.color = new Hsl(this.hue, randomInt(1, 100), randomInt(1, 100));
+        this.speed = randomInt(settings.minSpeed, settings.maxSpeed);
+        this.position.x = -this.radius;
+        this.position.y = randomInt(this.radius, this.canvas.height - this.radius);
+    }
+
+    update() {
+        this.position.x += this.speed;
+
+        if (this.position.x >= this.canvas.width + this.radius) {
+            this.initValues();
+        }
+    }
+
+    animate() {
+        this.update();
+        this.draw();
+    }
+}
---
+++ src/ts/Main.ts
@@ -0,0 +1,55 @@
+import {settings} from "./settings";
+import {randomInt} from "./framework25/helpers/random";
+import {FallingObstacle} from "./FallingObstacle";
+import {Animation} from "./framework25/Animation";
+import {isPointInCircle} from "./framework25/helpers/collision";
+
+class Main {
+    private readonly canvas: HTMLCanvasElement;
+    private readonly ctx: CanvasRenderingContext2D;
+    private readonly hue: number;
+    private readonly fallingObstacles: FallingObstacle[] = [];
+    private readonly animation: Animation;
+
+    constructor() {
+        this.canvas = document.getElementById(settings.canvasID) as HTMLCanvasElement;
+        this.ctx = this.canvas.getContext("2d");
+        this.hue = randomInt(1, 360);
+        this.addEventListeners();
+        this.resizeCanvas();
+        this.addFallingObstacles();
+        this.animation = new Animation(this.canvas, this.ctx, this.fallingObstacles);
+        this.animation.start();
+    }
+
+    private resizeCanvas() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+    }
+
+
+    private addEventListeners() {
+        window.addEventListener("resize", () => {
+            this.resizeCanvas();
+        });
+
+        this.canvas.addEventListener("click", (evt) => {
+            for (const fallingObstacle of this.fallingObstacles) {
+                if (isPointInCircle({
+                    x: evt.clientX,
+                    y: evt.clientY
+                }, fallingObstacle.position, fallingObstacle.radius)) {
+                    fallingObstacle.color = settings.redColor;
+                }
+            }
+        });
+    }
+
+    private addFallingObstacles() {
+        for (let i = 0; i < settings.maxFallingObstacles; i++) {
+            this.fallingObstacles.push(new FallingObstacle(this.ctx, this.canvas, this.hue));
+        }
+    }
+}
+
+new Main();
